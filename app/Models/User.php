769f5a8af0b31660000d8dde311f52837281eb70
JasonLWF <824759360@qq.com>
2018-08-03 07:30:41 +0000

    收货地址列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,4 +30,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function addresses()
+    {
+        return $this->hasMany(UserAddress::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::group(['middleware' => 'auth'], function() {
 	Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
 	Route::get('/email_verificaion/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
 	Route::group(['middleware' => 'email_verified'], function(){
-		
+		Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
 	});
 });
