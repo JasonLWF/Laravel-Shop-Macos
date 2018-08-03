59e97a79a53d99b56afbc8aa5f67a4c56b3fd081
JasonLWF <824759360@qq.com>
2018-08-03 03:46:38 +0000

    异常处理

--- app/Http/Controllers/EmailVerificationController.php
+++ app/Http/Controllers/EmailVerificationController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Cache;
 use Illuminate\Http\Request;
 use App\Notifications\EmailVerificationNotification;
+use App\Exceptions\InvalidRequestException;
 use Mail;
 
 class EmailVerificationController extends Controller
@@ -18,13 +19,13 @@ class EmailVerificationController extends Controller
     	$token = $request->input('token');
     	// 如果有一个为空说明不是一个合法的验证链接，直接抛出异常。
     	if (!$email || !$token) {
-    		throw new Exception("验证链接不正确");
+    		throw new InvalidRequestException("验证链接不正确");
     		
     	}
     	// 从缓存中读取数据，我们把从 url 中获取的 `token` 与缓存中的值做对比
         // 如果缓存不存在或者返回的值与 url 中的 `token` 不一致就抛出异常。
         if ($token != Cache::get('email_verification_'.$email)) {
-        	throw new Exception("验证链接不正确或已过期");
+        	throw new InvalidRequestException("验证链接不正确或已过期");
         	
         }
 
@@ -32,7 +33,7 @@ class EmailVerificationController extends Controller
         // 通常来说能通过 token 校验的情况下不可能出现用户不存在
         // 但是为了代码的健壮性我们还是需要做这个判断
         if (!$user = User::where('email', $email)->first()) {
-        	throw new Exception("用户不存在");
+        	throw new InvalidRequestException("用户不存在");
         }
         // 将指定的 key 从缓存中删除，由于已经完成了验证，这个缓存就没有必要继续保留。
         Cache::forget('email_verification_'.$email);
@@ -47,7 +48,7 @@ class EmailVerificationController extends Controller
         $user = $request->user();
         // 判断用户是否已经激活
         if ($user->email_verified) {
-            throw new Exception('您已经验证过邮箱了');
+            throw new InvalidRequestException('您已经验证过邮箱了');
             
         }
         // 调用 notify() 方法用来发送我们定义好的通知类
