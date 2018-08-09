d3f05bf94dae4336bf40c207aab06c56b9e6fbf8
JasonLWF <824759360@qq.com>
2018-08-09 03:13:47 +0000

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,8 @@ Route::group(['middleware' => 'auth'], function() {
 		Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 		Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 		Route::post('cart', 'CartController@add')->name('cart.add');
+		Route::get('cart', 'CartController@index')->name('cart.index');
+		Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 	});
 });
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
