b1501c820d4c749351c92fb911ac55792909436b
JasonLWF <824759360@qq.com>
2018-08-31 07:32:51 +0000

    优化优惠券模块

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -173,13 +173,16 @@
           if (error.response.status === 422) {
             // http 状态码为 422 代表用户输入校验失败
             var html = '<div>';
-            _.each(error.response.data.errors, function (errors) {
+            _.each(errors.response.data.errors, function (errors) {
               _.each(errors, function (error) {
-                html += error+'<br>';
+                html += errors+'<br>';
               })
             });
             html += '</div>';
             swal({content: $(html)[0], icon: 'error'})
+          } else if (error.response.status === 403) {
+            // 如果返回码是 403， 说明有其他条件不满足
+            swal(error.response.data.msg, '', 'error');
           } else {
             // 其他情况应该是系统挂了
             swal('系统错误', '', 'error');
